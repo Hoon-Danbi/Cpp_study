42eee0546c6c34efd757bba9ae8f525c0bdcb7e2
조재훈 <chojh0219@jojaehun-ui-MacBookAir.local>
2025-07-16 23:04:45 +0900

    feat: hello

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
+
+    int N;
+    cout<<"N:입력 해주세요 "<<'\n';
+    cin>>N;
+    cout<<"입력된 N: "<<N<<'\n';
+
     std::cout << "Hello worldqwdqwdqw!" << std::endl;
     return 0;
 }

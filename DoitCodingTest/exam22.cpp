9d91ea8510ce1b09443cd38b4a9b85341511a24c
b.hxxn <96778072+Hoon-Danbi@users.noreply.github.com>
2026-02-10 16:01:09 +0900

    오류 수정

--- DoitCodingTest/exam22.cpp
+++ DoitCodingTest/exam22.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 // 1. 숫자의 범위가 1~10,000이므로 크기가 10,001인 배열 선언
 // 전역 변수로 선언하면 자동으로 0으로 초기화됩니다.
-int count[10001];
+int cnt[10001];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -25,14 +25,14 @@ int main() {
     for (int i = 0; i < N; i++) {
         int num;
         cin >> num;
-        count[num]++; // num이라는 숫자가 나올 때마다 카운트 업
+        cnt[num]++; // num이라는 숫자가 나올 때마다 카운트 업
     }
 
     // 3. 1부터 10,000까지 돌면서 카운트된 횟수만큼 숫자 출력
     for (int i = 1; i <= 10000; i++) {
-        while (count[i] > 0) {
+        while (cnt[i] > 0) {
             cout << i << "\n";
-            count[i]--;
+            cnt[i]--;
         }
     }
 
